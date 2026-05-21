e69eb41e28b353fc428fcff321d6c0cff3e1c305
AlessandroPesole <alespesole@gmail.com>
2026-05-21 03:58:19 +0200

    feat: find event by id

--- src/main/java/event/service/EventService.java
+++ src/main/java/event/service/EventService.java
@@ -5,6 +5,7 @@ import event.dto.EventResponseDTO;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface EventService {
     EventResponseDTO insertEvent(EventRequestDTO event);
@@ -16,6 +17,8 @@ public interface EventService {
     EventResponseDTO selectEventById(int id);
 
     void deleteById(int id);
+
+    Optional<EventResponseDTO> findById(int id);
 
     boolean existsById(int id);
 
